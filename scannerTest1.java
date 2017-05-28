95f41c1270ee60016680b72425bfa5cfb4610729
Kylle <kyllecolumna@users.noreply.github.com>
2017-05-28 08:32:00 +0800

    Update scannerTest1.java

--- scannerTest1.java
+++ scannerTest1.java
@@ -11,129 +11,120 @@ import java.util.*;
 public class scannerTest1 {
 	public static void main(String[] args) {
 		
-	// place content from file to open into a string
-	String fileName = "C:\\Users\\Bea Mariano\\Desktop\\Codes\\FIRE EXIT MP\\123_MP-master\\MP.txt";
+		// place content from file to open into a string
+		String fileName = "C:\\Users\\Bea Mariano\\Desktop\\Codes\\FIRE EXIT MP\\123_MP-master\\MP.txt";
 
-        // reference one line at a time
-        String line = null;
-        String line1 = null;
+		// reference one line at a time
+		String line = null;
+		String line1 = null;
 
-        try {
-        	
-        	// FileReader reads text files in the default encoding.
-            	FileReader fileReader = new FileReader(fileName);
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(fileName);
 
-            	// Always wrap FileReader in BufferedReader.
-            	BufferedReader bufferedReader = new BufferedReader(fileReader);
-            	//Scanner input = new Scanner(fileName);
-            	int floorCount = 0;
-            	int arrayDumpCount = 0;
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			//Scanner input = new Scanner(fileName);
+			int floorCount = 0;
+			int arrayDumpCount = 0;
 
-            while((line = bufferedReader.readLine()) != null) {
-                int count = 0;
-                
-                for (int i = 0, len = line.length(); i < len; i++) {
-                    if (Character.isWhitespace(line.charAt(i))) {
-                        count++;
-                    }
-                }
-                
-                if (count == 1) {
-                	floorCount++;
-                } else if (count == 2) {
-                	arrayDumpCount++;;
-                }
-            
-            }
-            
-            // Always close files.
-            bufferedReader.close();
-            
-            System.out.println("floorCount: " + floorCount);
-            System.out.println("arrayDumpCount: " + arrayDumpCount);
-            
-            int count1 = 0;
-            int count2 = 0;
-            
-            String[] twoDigitArray = new String[floorCount];
-            String[] threeDigitArray = new String[arrayDumpCount];
-            
-            String fName = "C:\\Users\\kyllecolumna\\Desktop\\MP.txt";
-            FileReader fReader = new FileReader(fName);
-            BufferedReader br = new BufferedReader(fReader);
-            while ((line1 = br.readLine()) != null) {
-            	int count = 0;
-                for (int i = 0, len = line1.length(); i < len; i++) {
-                    if (Character.isWhitespace(line1.charAt(i))) {
-                        count++;
-                    }
-                }
-                
-                if (count == 1) {
-                	twoDigitArray[count1] = line1;
-                	count1++;
-                } else if (count == 2) {
-                	threeDigitArray[count2] = line1;
-                	count2++;
-                }
-            }
-            
-            br.close();
-            
-            int[] roomCount = new int[floorCount]; 
-            int[] pathCount = new int[floorCount];
-            
-            for (int i = 0; i < floorCount; i++) {
-            	Scanner input = new Scanner(twoDigitArray[i]);
-            	roomCount[i] = input.nextInt();
-            	pathCount[i] = input.nextInt();
-            	System.out.println(twoDigitArray[i]);
-            }
-            
-            Graph.Edge[] edgeGroup;
-            edgeGroup = new Graph.Edge[arrayDumpCount];
-                      
-            String START, END;
-		
-            List<Integer> x = new ArrayList<Integer>();    	
-            Graph.Edge[] GRAPH = {};
-            	
-            // ERROR: The method Edge(String, String, int) is undefined for the type Graph
-            for (int j = 0; j < arrayDumpCount; j++) {
-            	Scanner input2 = new Scanner(threeDigitArray[j]);
-             	String temp1 = String.valueOf(input2.nextInt());
-             	String temp2 = String.valueOf(input2.nextInt());
-            	GRAPH.add(Graph.Edge(temp1, temp2, input2.nextInt()));
-            }
-            	
-            	            	
-            for (int y = 0; y < floorCount; y++) {
-            	for (int k = 0; k < (roomCount[y]); k++) {
-            		String temp = String.valueOf(k);
-            		START = temp;
-            		String temp1 = String.valueOf(roomCount[y] - 1);
-            		END = temp1;
-                	Graph g = new Graph(GRAPH);
-                    g.dijkstra(START);
-                    g.printPath(END);
-                }
-            }
-            	
-            	
-            	
-		
-            	
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println(
-                "Unable to open file '" +
-                fileName + "'");
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error reading file '"
-                + fileName + "'");
-        }
+			while((line = bufferedReader.readLine()) != null) {
+				int count = 0;
+
+				for (int i = 0, len = line.length(); i < len; i++) {
+					if (Character.isWhitespace(line.charAt(i))) {
+						count++;
+					}
+				}
+
+				if (count == 1) {
+					floorCount++;
+				} else if (count == 2) {
+					arrayDumpCount++;;
+				}
+
+			}
+
+			// Always close files.
+			bufferedReader.close();
+
+			System.out.println("floorCount: " + floorCount);
+			System.out.println("arrayDumpCount: " + arrayDumpCount);
+
+			int count1 = 0;
+			int count2 = 0;
+
+			String[] twoDigitArray = new String[floorCount];
+			String[] threeDigitArray = new String[arrayDumpCount];
+
+			String fName = "C:\\Users\\kyllecolumna\\Desktop\\MP.txt";
+			FileReader fReader = new FileReader(fName);
+			BufferedReader br = new BufferedReader(fReader);
+
+			while ((line1 = br.readLine()) != null) {
+				int count = 0;
+
+				for (int i = 0, len = line1.length(); i < len; i++) {
+					if (Character.isWhitespace(line1.charAt(i))) {
+						count++;
+					}
+				}
+
+				if (count == 1) {
+					twoDigitArray[count1] = line1;
+					count1++;
+				} else if (count == 2) {
+					threeDigitArray[count2] = line1;
+					count2++;
+				}
+			}
+
+			br.close();
+
+			int[] roomCount = new int[floorCount]; 
+			int[] pathCount = new int[floorCount];
+
+			for (int i = 0; i < floorCount; i++) {
+				Scanner input = new Scanner(twoDigitArray[i]);
+				roomCount[i] = input.nextInt();
+				pathCount[i] = input.nextInt();
+				System.out.println(twoDigitArray[i]);
+			}
+
+			Graph.Edge[] edgeGroup;
+			edgeGroup = new Graph.Edge[arrayDumpCount];
+
+			String START, END;
+
+			List<Integer> x = new ArrayList<Integer>();    	
+			Graph.Edge[] GRAPH = {};
+
+			// ERROR: The method Edge(String, String, int) is undefined for the type Graph
+			for (int j = 0; j < arrayDumpCount; j++) {
+				Scanner input2 = new Scanner(threeDigitArray[j]);
+				String temp1 = String.valueOf(input2.nextInt());
+				String temp2 = String.valueOf(input2.nextInt());
+				GRAPH.add(Graph.Edge(temp1, temp2, input2.nextInt()));
+			}
+
+
+			for (int y = 0; y < floorCount; y++) {
+				for (int k = 0; k < (roomCount[y]); k++) {
+					String temp = String.valueOf(k);
+					START = temp;
+					String temp1 = String.valueOf(roomCount[y] - 1);
+					END = temp1;
+					Graph g = new Graph(GRAPH);
+					g.dijkstra(START);
+					g.printPath(END);
+				}
+			}
+
+		} catch(FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch(IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+		}
 	}
 }
 
